0a065512cff24d8a16f1145fb8d519bea0ac5024
josephang <josephang05@gmail.com>
2016-10-24 22:02:24 -0700

    First Commit

---
+++ empressyelizaveta.com/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<meta http-equiv="refresh" content="2;url=index.html">
+<html lang="en">
+
+<head>
+  <title>Empress Yelizaveta</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css\front.css">
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Empress Yelizaveta</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+	  <li><a href="page2.html">Dog Riding A Unicycle</a></li>
+      <li><a href="page3.html">The Plant Cam</a></li>
+      <li><a href="#" onClick="passWord()" >Not Secret Plans</a></li>
+
+    </ul>
+  </div>
+</nav>
+
+</body>
+</html>
+
+<footer>
+	<script src="js\passuser.js"> </script>
+</footer>
---
+++ nextcloud/version.php
@@ -0,0 +1,7 @@
+<?php 
+$OC_Version = array(9,1,0,16);
+$OC_VersionString = '10.0';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_VersionCanBeUpgradedFrom = array(9,0);
+$OC_Build = '2016-08-25T04:01:11+00:00 678fce3c18c6fbed60076764ae98171eae617c05';
